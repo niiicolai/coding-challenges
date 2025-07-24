de91cedb488c06a496cd4e14c2020cf4e8ff3a9c
niiicolai <niiicolai@gmail.com>
2025-07-24 03:14:51 +0200

    use hash router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import { routes } from './routes';
 
-export const router = createBrowserRouter(routes);
+export const router = createHashRouter(routes);
